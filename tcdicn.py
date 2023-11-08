cde6c691758f4ed4b7d2b586bca85d8d213597eb
tedski999 <ski@h8c.de>
2023-11-08 01:20:26 +0000

    Clean up peer addressing
    Added _Address class
    Replaced instances of (host, port) with _Address
    Replaced _send_to_peers with _send_to_addrs to allow arbitrary destination addresses

--- tcdicn.py
+++ tcdicn.py
@@ -10,6 +10,22 @@ from typing import Tuple, Dict
 VERSION = "0.1"
 
 
+class _Address:
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def __eq__(self, other):
+        return self.host == other.host and self.port == other.port
+
+    def __hash__(self):
+        return hash((self.host, self.port))
+
+    def __str__(self):
+        return f"{self.host}:{self.port}"
+
+
 class Server:
 
     # Initialise server
@@ -18,7 +34,7 @@ class Server:
         self.announcement = self._create_announcement_msg()  # Bytes to send as announcement
         self.announcement_interval = announcement_interval  # Seconds between each announcement
         self.peer_timeout_interval = peer_timeout_interval  # Seconds without announcement until a peer is forgotten
-        self.peers: Dict[Tuple[str, int], Task] = dict()  # Cache of peers whose announcement we have recently heard
+        self.peers: Dict[_Address, Task] = dict()  # Cache of peers whose announcement we have recently heard
         self.dataset = dict()  # Local named data cache
 
     # Start server
@@ -38,7 +54,7 @@ class Server:
     # Publish named data to the ICN network
     async def set(self, name: str, value: str):
         self.dataset[name] = { "value": value, "time": time.time() }
-        await self._send_to_peers(self._create_set_msg(name))
+        await self._send_to_addrs(self._create_set_msg(name), self.peers.keys())
 
     # Listen for and regularly broadcast peer announcements to let other nodes know we exist
     async def _start_udp_server(self):
@@ -50,7 +66,7 @@ class Server:
             def connection_lost(_, e: Exception):
                 logging.warning(f"UDP transport lost: {e}")
             def datagram_received(_, data: bytes, addr: Tuple[str, int]):
-                self._on_udp_datagram(data, *addr)
+                self._on_udp_datagram(data, _Address(*addr[0:2]))
             def error_received(_, e: OSError):
                 logging.warning(f"UDP transport error: {e}")
         transport, protocol = await asyncio.get_running_loop().create_datagram_endpoint(
@@ -63,10 +79,10 @@ class Server:
 
     # If we receive an announcement from a peer, add its address to our peer cache with an expiry timer
     # If it is already in our peer cache, reset its expiry timer
-    def _on_udp_datagram(self, data: bytes, host: str, port: int):
+    def _on_udp_datagram(self, data: bytes, addr: _Address):
         # Ignore own announcements, adapted from https://gist.github.com/bennr01/7043a460155e8e763b3a9061c95faaa0
         local_addrs = socket.getaddrinfo(socket.gethostname(), self.port)
-        remote_addrs = socket.getaddrinfo(socket.getfqdn(host), port)
+        remote_addrs = socket.getaddrinfo(socket.getfqdn(addr.host), addr.port)
         for (_, _, _, _, local_addr) in local_addrs:
             for (_, _, _, _, remote_addr) in remote_addrs:
                 if remote_addr == local_addr:
@@ -75,17 +91,17 @@ class Server:
         if data != self.announcement:
             return
         # Cancel previous expiry timer
-        logging.debug(f"Received peer announcement from {host}:{port}.")
-        if (host, port) in self.peers:
-            self.peers[(host, port)].cancel()
+        logging.debug(f"Received peer announcement from {addr}.")
+        if addr in self.peers:
+            self.peers[addr].cancel()
         else:
-            logging.info(f"Added new peer: {host}:{port}.")
+            logging.info(f"Added new peer: {addr}.")
         # Insert new peer address and expiry timer
         async def _do_peer_timeout():
             await asyncio.sleep(self.peer_timeout_interval)
-            logging.info(f"Removed timed-out peer: {host}:{port}.")
-            del self.peers[(host, port)]
-        self.peers[(host, port)] = asyncio.create_task(_do_peer_timeout())
+            logging.info(f"Removed timed-out peer: {addr}.")
+            del self.peers[addr]
+        self.peers[addr] = asyncio.create_task(_do_peer_timeout())
 
     # Start listening for peer connecting to get or set named data on the network
     async def _start_tcp_server(self):
@@ -95,9 +111,11 @@ class Server:
 
     # Handle peer connection
     async def _on_tcp_connection(self, reader: StreamReader, writer: StreamWriter):
-        logging.debug("Handling new TCP connection...")
+        addr = _Address(*writer.get_extra_info("sockname")[0:2])
+        logging.debug(f"Handling TCP connection from {addr}...")
         # Read entire message
         msg_bytes = await reader.read()
+        writer.close()
         msg = json.loads(msg_bytes)
         # Ignore invalid messages
         if msg["version"] != VERSION or msg["type"] != "set":
@@ -106,21 +124,23 @@ class Server:
         name = msg["name"]
         data = { "value": msg["value"], "time": msg["time"] }
         # Ignore duplicate messages
-        if name in self.dataset and data["time"] == self.dataset[name]["time"]:
+        if name in self.dataset and data["time"] <= self.dataset[name]["time"]:
+            logging.debug(f"Ignoring duplicate data {name} from {addr}: {data}")
             return
-        # Insert new data into data
+        # Insert new data into data and forward to other peers
+        logging.info(f"Received update to data {name} from {addr}: {data}")
         self.dataset[name] = data
-        # Tell other peers
-        await self._send_to_peers(msg_bytes)
+        other_addrs = [ peer for peer in self.peers.keys() if peer != addr ]
+        await self._send_to_addrs(msg_bytes, other_addrs)
 
-    # Send bytes over TCP to every known peer
-    async def _send_to_peers(self, msg_bytes: bytes):
-        async def send(host, port):
-            logging.debug(f"Sending message to {host}:{port}...")
-            reader, writer = await asyncio.open_connection(host, port)
+    # Send bytes over TCP to list of addresses
+    async def _send_to_addrs(self, msg_bytes: bytes, addrs):
+        async def send(addr):
+            logging.debug(f"Sending message to {addr}...")
+            reader, writer = await asyncio.open_connection(addr.host, addr.port)
             writer.write(msg_bytes)
             writer.close()
-        aws = [ asyncio.create_task(send(*addr)) for addr in self.peers.keys() ]
+        aws = [ asyncio.create_task(send(addr)) for addr in addrs ]
         if len(aws) != 0:
             _, pending = await asyncio.wait(aws, timeout=10)
             if len(pending) != 0:
