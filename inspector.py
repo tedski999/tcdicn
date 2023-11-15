5a8aa5c0fb69d726174ee73aeb587583adb652b2
Pragatiaboti <67864526+Pragatiaboti@users.noreply.github.com>
2023-11-15 23:47:56 +0000

    Update inspector.py

--- inspector.py
+++ inspector.py
@@ -1,64 +1,17 @@
-# import asyncio
-# import json
-# from tcdicn import Server
-
-# # Define the inspector
-# class Inspector:
-#     def __init__(self, server):
-#         self.server = server
-#         self.drones = ["drone06", "drone07", "drone08", "drone09", "drone10"]  # List of drones to monitor
-
-#     async def monitor_drones(self):
-#         while True:
-#             for drone_id in self.drones:
-#                 # Subscribe to sensor data for each drone
-#                 position = await self.server.get(f"{drone_id}-position")
-#                 temperature = await self.server.get(f"{drone_id}-temperature")
-#                 battery = await self.server.get(f"{drone_id}-battery")
-
-#                 # Process sensor data - Battery
-#                 if battery and float(battery) < 10:
-#                     # Publish alert for low battery
-#                     await self.server.set(f"status-{drone_id}", "battery-low")
-
-#                 # Process sensor data - Temperature
-#                 if temperature and float(temperature) > 50:
-#                     # Publish alert for temperature
-#                     await self.server.set(f"status-{drone_id}", "Temperature-alert")
-
-#                 # Process sensor data - position
-#                 if (position[0] and float(position[0]) < 0.01 or position[0]  and float(position[0]) > 10.0) and (position[1]  and float(position[1]) < 0.01 or position[1]  and float(position[1]) > 10.0):
-#                     # Publish alert for position
-#                     await self.server.set(f"status-{drone_id}", "Position-alert")
-
-#                 # Here you could add more complex logic or ML model predictions
-
-#             # Wait a bit before next monitoring cycle
-#             await asyncio.sleep(10)
-
-# async def main():
-#     server = Server(port=33333, announce_interval=60, peer_timeout=180)
-#     inspector = Inspector(server)
-#     await server.start()
-#     await inspector.monitor_drones()
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
-
-
 import asyncio
 import json
 import random
 import numpy as np
-from tcdicn import Server
+from tcdicn import Node
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-class Inspector:
-    def __init__(self, server):
+class DroneMonitorNode(Node):
+    def __init__(self, drone_id, server, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.drone_id = drone_id
         self.server = server
-        self.drones = ["drone06", "drone07", "drone08", "drone09", "drone10"]
         self.model = DecisionTreeClassifier(random_state=42)
         self.feature_columns = ["temperature", "battery", "position_0", "position_1"]
         self.target_column = "failure"
@@ -74,31 +27,30 @@ class Inspector:
         accuracy = accuracy_score(y_test, predictions)
         print(f"ML Model trained with accuracy: {accuracy}")
 
-    async def monitor_drones(self):
+    async def monitor_drone(self):
         await self.train_ml_model()  # Train the ML model initially
         while True:
-            for drone_id in self.drones:
-                # Subscribe to sensor data for each drone
-                position = await self.server.get(f"{drone_id}-position")
-                temperature = await self.server.get(f"{drone_id}-temperature")
-                battery = await self.server.get(f"{drone_id}-battery")
+            # Subscribe to sensor data for the drone
+            position = await self.get(f"{self.drone_id}-position")
+            temperature = await self.get(f"{self.drone_id}-temperature")
+            battery = await self.get(f"{self.drone_id}-battery")
 
-                # Process sensor data
-                if all(x is not None for x in [position, temperature, battery]):
-                    # Prepare features for prediction
-                    features = {
-                        "temperature": float(temperature),
-                        "battery": float(battery),
-                        "position_0": float(position[0]),
-                        "position_1": float(position[1]),
-                    }
+            # Process sensor data
+            if all(x is not None for x in [position, temperature, battery]):
+                # Prepare features for prediction
+                features = {
+                    "temperature": float(temperature),
+                    "battery": float(battery),
+                    "position_0": float(position[0]),
+                    "position_1": float(position[1]),
+                }
 
-                    # Make prediction
-                    failure_prediction = self.model.predict([list(features.values())])[0]
+                # Make prediction
+                failure_prediction = self.model.predict([list(features.values())])[0]
 
-                    # Publish alert based on prediction
-                    if failure_prediction == 1:
-                        await self.server.set(f"status-{drone_id}", "Failure-alert")
+                # Publish alert based on prediction
+                if failure_prediction == 1:
+                    await self.server.set(f"status-{self.drone_id}", "Failure-alert")
 
             # Wait a bit before the next monitoring cycle
             await asyncio.sleep(10)
@@ -135,10 +87,14 @@ class Inspector:
         return historical_data
 
 async def main():
-    server = Server(port=33333, announce_interval=60, peer_timeout=180)
-    inspector = Inspector(server)
-    await server.start()
-    await inspector.monitor_drones()
+    server = Node()
+    await server.start(port=33333, dport=33334, ttl=180, tpf=60, client={"name": "inspector", "labels": ["failure-alert"]})
+
+    drones = ["drone06", "drone07", "drone08", "drone09", "drone10"]
+    drone_nodes = [DroneMonitorNode(drone_id, server) for drone_id in drones]
+
+    tasks = [node.monitor_drone() for node in drone_nodes]
+    await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     asyncio.run(main())
