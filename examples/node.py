6d66cbf9f60bfa9ae09983704e7020d11cd0cfe1
tedski999 <ski@h8c.de>
2023-11-15 09:26:37 +0000

    Fix dumb log formatting bug

--- examples/node.py
+++ examples/node.py
@@ -14,7 +14,7 @@ async def main():
 
     # Logging verbosity
     logging.basicConfig(
-        format="%(asctime)s.%(msecs)04d [%(levelname)s] %(message)s",
+        format="%(asctime)s.%(msecs)03d [%(levelname)s] %(message)s",
         level=logging.DEBUG, datefmt="%H:%M:%S:%m")
 
     # Run the ICN node until shutdown
