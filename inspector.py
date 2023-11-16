b391c69279161e8aa647c38f6c67c04fa3443e17
Pragatiaboti <67864526+Pragatiaboti@users.noreply.github.com>
2023-11-16 08:24:45 -0500

    Update inspector.py

--- inspector.py
+++ inspector.py
@@ -6,95 +6,82 @@ from tcdicn import Node
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import time
+
+UNRESPONSIVE_THRESHOLD = 60  #(in seconds)
+UNRESPONSIVE_CHECK_INTERVAL = 10 #(in seconds)
 
 class DroneMonitorNode(Node):
-    def __init__(self, drone_id, server, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def _init_(self, drone_id, server, ttp=5, *args, **kwargs):
+        super()._init_(*args, **kwargs)
         self.drone_id = drone_id
         self.server = server
-        self.model = DecisionTreeClassifier(random_state=42)
-        self.feature_columns = ["temperature", "battery", "position_0", "position_1"]
-        self.target_column = "failure"
+        self.ttp = ttp  # Time To Propagate
+        self.last_received_message_time = time.time()
 
-    async def train_ml_model(self):
-        # Train the ML model using historical data
-        historical_data = await self._collect_historical_data()
-        X_train, X_test, y_train, y_test = train_test_split(
-            historical_data[self.feature_columns], historical_data[self.target_column], test_size=0.2, random_state=42
-        )
-        self.model.fit(X_train, y_train)
-        predictions = self.model.predict(X_test)
-        accuracy = accuracy_score(y_test, predictions)
-        print(f"ML Model trained with accuracy: {accuracy}")
-
-    async def monitor_drone(self):
-        await self.train_ml_model()  # Train the ML model initially
+    async def detect_unresponsive(self):
         while True:
-            # Subscribe to sensor data for the drone
-            position = await self.get(f"{self.drone_id}-position")
-            temperature = await self.get(f"{self.drone_id}-temperature")
-            battery = await self.get(f"{self.drone_id}-battery")
+            # Check if a drone has not sent a message within the specified time
+            if (
+                self.last_received_message_time is not None
+                and time.time() - self.last_received_message_time > UNRESPONSIVE_THRESHOLD
+            ):
+                # Drone is unresponsive, notify controllers
+                await self.server.set(f"status-{self.drone_id}", "Unresponsive")
+            await asyncio.sleep(UNRESPONSIVE_CHECK_INTERVAL)
 
-            # Process sensor data
-            if all(x is not None for x in [position, temperature, battery]):
-                # Prepare features for prediction
-                features = {
-                    "temperature": float(temperature),
-                    "battery": float(battery),
-                    "position_0": float(position[0]),
-                    "position_1": float(position[1]),
-                }
+    async def monitor_drones(self):
+        asyncio.create_task(self.detect_unresponsive())  # Start the unresponsive detection task
 
-                # Make prediction
-                failure_prediction = self.model.predict([list(features.values())])[0]
+        history_buffer = []  # Buffer to store the last 100 readings
+        while True:
+            for drone_id in self.drones:
+                # Subscribe to different types of sensor data for each drone separately
+                position = await self.get(f"{drone_id}-position")
+                temperature = await self.get(f"{drone_id}-temperature")
+                battery = await self.get(f"{drone_id}-battery")
 
-                # Publish alert based on prediction
-                if failure_prediction == 1:
-                    await self.server.set(f"status-{self.drone_id}", "Failure-alert")
+                # Process sensor data and update history buffer
+                if all(x is not None for x in [position, temperature, battery]):
+                    features = {
+                        "temperature": float(temperature),
+                        "battery": float(battery),
+                        "position_0": float(position[0]),
+                        "position_1": float(position[1]),
+                    }
+                    history_buffer.append(features)
+                    if len(history_buffer) > 100:
+                        history_buffer.pop(0)
+
+            # Run ML model on the last 5 minutes of collected data
+            recent_data = history_buffer[-300:]  # 300 seconds = 5 minutes
+            if len(recent_data) > 0:
+                features_matrix = [list(features.values()) for features in recent_data]
+                failure_predictions = self.model.predict(features_matrix)
+
+                # Publish alerts based on predictions
+                for idx, failure_prediction in enumerate(failure_predictions):
+                    if failure_prediction == 1:
+                        await self.server.set(f"status-{self.drone_id}", "Failure-alert")
 
             # Wait a bit before the next monitoring cycle
             await asyncio.sleep(10)
 
-    async def _collect_historical_data(self):
-        historical_data = {
-            "temperature": [],
-            "battery": [],
-            "position_0": [],
-            "position_1": [],
-            "failure": [],
-        }
+    async def on_data(self, data, meta):
+        # Update the last received message time when data is received
+        self.last_received_message_time = time.time()
+        await super().on_data(data, meta)
 
-        # Simulate data for 100 time steps
-        for _ in range(100):
-            # Simulate normal operating conditions
-            temperature = np.random.normal(loc=25, scale=5)
-            battery = np.random.uniform(low=20, high=100)
-            position_0 = np.random.uniform(low=0, high=10)
-            position_1 = np.random.uniform(low=0, high=10)
-            failure = 0  # No failure
-
-            # Introduce failure scenarios randomly
-            if random.random() < 0.1:  # 10% chance of failure
-                failure = 1  # Failure
-
-            # Append data to historical_data
-            historical_data["temperature"].append(temperature)
-            historical_data["battery"].append(battery)
-            historical_data["position_0"].append(position_0)
-            historical_data["position_1"].append(position_1)
-            historical_data["failure"].append(failure)
-
-        return historical_data
-
+# Main Function
 async def main():
     server = Node()
-    await server.start(port=33333, dport=33334, ttl=180, tpf=60, client={"name": "inspector", "labels": ["failure-alert"]})
+    await server.start(port=33333, dport=33334, ttl=180, tpf=60, client={"name": "inspector", "labels": ["failure-alert"], "ttp": 5})
 
     drones = ["drone06", "drone07", "drone08", "drone09", "drone10"]
     drone_nodes = [DroneMonitorNode(drone_id, server) for drone_id in drones]
 
-    tasks = [node.monitor_drone() for node in drone_nodes]
+    tasks = [node.monitor_drones() for node in drone_nodes]
     await asyncio.gather(*tasks)
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     asyncio.run(main())
